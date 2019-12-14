b7a5f54b87f8d4b9c90cf6170b5daa2abce9ebca
Nicola Sacco <n.sacco.88@gmail.com>
2019-12-14 23:02:46 +0100

    Updated to latest version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='django_remote_queryset',
-    version='0.1.2',
+    version='0.1.3',
     scripts=[
     ],
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
